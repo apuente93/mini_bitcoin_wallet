2182f64bd93329df81ff4955fa662bae21b49967
Alejandro Puente <alejandropuente@Alejandros-MacBook-Pro.local>
2024-08-07 15:28:33 -0600

    Fixed a bug with /dashboard redirect after sign in

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,4 +1,3 @@
-// src/components/AuthProvider.tsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { auth } from '../firebaseConfig';
 import { User } from 'firebase/auth';
@@ -7,16 +6,19 @@ interface AuthContextType {
   currentUser: User | null;
   setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>;
   logout: () => Promise<void>;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
@@ -25,6 +27,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     currentUser,
     setCurrentUser,
     logout: () => auth.signOut(),
+    loading,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,18 @@
-// src/components/ProtectedRoute.tsx
 import React, { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
+import Spinner from 'react-bootstrap/Spinner';
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { currentUser } = useAuth();
+  const { currentUser, loading } = useAuth();
+
+  if (loading) {
+    return <div className="center-spinner"><Spinner animation="border" /></div>;
+  }
 
   return currentUser ? <>{children}</> : <Navigate to="/signin" />;
 };
